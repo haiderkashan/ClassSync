55a7c0a13416937e605738f25d7d2bc19a363044
Kashan Haider <kashanhaiderinreallife@gmail.com>
2026-09-26 11:03:29 +0500

    chore(release): configure production console log stripping and bundle optimizations

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,16 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+// Optimize production minification: strip debug console logs while preserving warn/error
+if (process.env.NODE_ENV === 'production') {
+  config.transformer = {
+    ...config.transformer,
+    minifierConfig: {
+      compress: {
+        drop_console: ['log', 'debug', 'info'],
+      },
+    },
+  };
+}
+
 module.exports = withNativeWind(config, { input: "./global.css" });
